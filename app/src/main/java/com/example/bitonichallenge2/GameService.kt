4fa1646da23606c06f17a4c5ee077ba1fe558354
Chris <xristoskarasoulas@gmail.com>
2022-03-17 00:51:09 +0200

    Added LocationCallbacks and Listeners Next : Update cursor and camera with my location

--- app/src/main/java/com/example/bitonichallenge2/GameService.kt
+++ app/src/main/java/com/example/bitonichallenge2/GameService.kt
@@ -1,33 +1,42 @@
 package com.example.bitonichallenge2
 
+import android.annotation.SuppressLint
 import android.app.NotificationChannel
 import android.app.NotificationManager
 import android.app.PendingIntent
 import android.content.Context
 import android.content.Intent
 import android.os.Build
+import android.os.Looper
 import android.util.Log
 import androidx.annotation.RequiresApi
 import androidx.core.app.NotificationCompat
 import androidx.lifecycle.LifecycleService
 import androidx.lifecycle.MutableLiveData
 import com.example.bitonichallenge2.model.*
+import com.google.android.gms.location.*
 import com.google.android.gms.maps.model.LatLng
 
 class GameService: LifecycleService() {
     var isFirstGame = true
+    lateinit var fusedLocationProviderClient: FusedLocationProviderClient
     companion object{
         var isGameOngoing = MutableLiveData<Boolean>()
-        var coordinates = MutableLiveData<List<LatLng>>()
-
-
+        var coordinates = MutableLiveData<LatLng>()
     }
 
+    override fun onCreate() {
+        super.onCreate()
+        postInitialValues()
+        fusedLocationProviderClient = FusedLocationProviderClient(this)
+
+        isGameOngoing.observe(this,{
+            updateLocation(it)
+        })
+    }
     private fun postInitialValues(){
-
         isGameOngoing.postValue(false)
-        coordinates.postValue(mutableListOf())
-
+        coordinates.postValue(LatLng(0.0,0.0))
     }
 
     // See https://www.youtube.com/watch?v=JpVBPKf2mIU&list=PLQkwcJG4YTCQ6emtoqSZS2FVwZR9FT3BV
@@ -47,7 +56,6 @@ class GameService: LifecycleService() {
                         Log.d("GameService","Resuming Service")
                     }
                 }
-
                 ACTION_PAUSE_SERVICE -> {
                     Log.d("GameService","Paused")}
                 ACTION_STOP_SERVICE -> {
@@ -58,8 +66,41 @@ class GameService: LifecycleService() {
         }
         return super.onStartCommand(intent, flags, startId)
     }
+    @SuppressLint("MissingPermission")
+    private fun updateLocation(isGame:Boolean){
+        if(isGame){
+            if(Utils.hasPermissions(this)){
+                val request = LocationRequest.create().apply {
+                    interval = 5000L
+                    fastestInterval = 2000L
+                    priority = LocationRequest.PRIORITY_HIGH_ACCURACY
+                }
+                fusedLocationProviderClient.requestLocationUpdates(
+                        request,
+                        lastLocation,
+                        Looper.getMainLooper()
+                )
+            }
+        }else{
+            fusedLocationProviderClient.removeLocationUpdates(lastLocation)
+        }
+    }
+    val lastLocation = object : LocationCallback(){
+        override fun onLocationResult(locationResult: LocationResult) {
+            super.onLocationResult(locationResult)
+            Log.d("GameService","Lat:${locationResult.lastLocation.latitude} Lon:${locationResult.lastLocation.longitude}")
+            if(isGameOngoing.value!!){
+                coordinates.postValue(LatLng(locationResult.lastLocation.latitude,locationResult.lastLocation.longitude))
+            }
+        }
+
+        override fun onLocationAvailability(p0: LocationAvailability) {
+            super.onLocationAvailability(p0)
+        }
+    }
 
     private fun startForegroundService(){
+        isGameOngoing.postValue(true)
         val notificationManager = getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
 
         if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.O){
